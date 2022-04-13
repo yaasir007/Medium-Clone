2cc0f83115d1aafdfa372869abcf31a2070eb5d1
Yaasir Cheekoory <yaasir1997@gmail.com>
2022-04-13 21:53:28 +0400

    added routes for users

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,14 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: 'pages#home'
+  # root to: 'articles#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  devise_scope :user do
+    authenticated :user do
+      root "articles#index", as: :authenticated_root
+    end
+    unauthenticated do
+      root "pages#home", as: :unauthenticated_root
+    end
+  end
 end
